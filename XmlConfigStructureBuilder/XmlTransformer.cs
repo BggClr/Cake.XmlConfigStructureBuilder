2517ccdff46470ffc2d5011ef3dab0c70130ca43
jincod <vadim.abdrashitov@gmail.com>
2017-08-09 12:52:30 +0500

    use new project files

--- XmlConfigStructureBuilder/XmlTransformer.cs
+++ XmlConfigStructureBuilder/XmlTransformer.cs
@@ -8,14 +8,16 @@ namespace XmlConfigStructureBuilder
 		public void Transform(string sourceFile, string transformFile, string targetFile)
 		{
 			using (var document = new XmlTransformableDocument {PreserveWhitespace = true})
-			using (var transform = new XmlTransformation(transformFile))
 			{
-				document.Load(sourceFile);
+				using (var transform = new XmlTransformation(transformFile))
+				{
+					document.Load(sourceFile);
 
-				if (!transform.Apply(document))
-					throw new Exception($"Failed to transform \"{sourceFile}\" using \"{transformFile}\" to \"{targetFile}\"");
+					if (!transform.Apply(document))
+						throw new Exception($"Failed to transform \"{sourceFile}\" using \"{transformFile}\" to \"{targetFile}\"");
 
-				document.Save(targetFile);
+					document.Save(targetFile);
+				}
 			}
 		}
 	}
