de9c94b5297f38e306e5c17a13ef6ca8de852675
jincod <vadim.abdrashitov@gmail.com>
2017-08-14 13:46:44 +0500

    added xml documentation comments

--- Cake.XmlConfigStructureBuilder/XmlConfigStructureBuilderAliases.cs
+++ Cake.XmlConfigStructureBuilder/XmlConfigStructureBuilderAliases.cs
@@ -6,10 +6,28 @@ using XmlConfigStructureBuilder;
 
 namespace Cake.XmlConfigStructureBuilder
 {
+	/// <summary>
+	/// The project compiles multiple xml configs to single one using the XDT transformation
+	/// </summary>
 	[CakeAliasCategory("Configuration")]
 	[CakeNamespaceImport("XmlConfigStructureBuilder")]
 	public static class XmlConfigStructureBuilderAliases
 	{
+		/// <summary>
+        /// Make configs inside project root
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// Task("MakeConfigs")
+        ///   .Does(() => {
+        ///     MakeConfigs("Release", ".");
+        /// });
+        /// </code>
+        /// </example>
+        /// <param name="context">The context.</param>
+        /// <param name="buildConfiguration">Build configuration. "Release" or "Debug" for example.</param>
+        /// <param name="projectRoot">Project root. "." for example.</param>
+        /// <param name="fileNameTemplatesFactory"></param>
 		[CakeMethodAlias]
 		public static void MakeConfigs(this ICakeContext context, string buildConfiguration, string projectRoot,
 			Func<string, string, bool, IEnumerable<string>> fileNameTemplatesFactory = null)
@@ -17,6 +35,22 @@ namespace Cake.XmlConfigStructureBuilder
 			Builder.CompileProjectConfigs(buildConfiguration, projectRoot, fileNameTemplatesFactory);
 		}
 
+		/// <summary>
+        /// Make single config file
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// Task("MakeConfigs")
+        ///   .Does(() => {
+        ///     MakeSingleConfig("Release", "ConnectionStrings", ".\\Web\\App_Config\\");
+        /// });
+        /// </code>
+        /// </example>
+        /// <param name="context">The context.</param>
+        /// <param name="buildConfiguration">Build configuration. "Release" or "Debug" for example.</param>
+        /// <param name="configName"></param>
+        /// <param name="configDir">Project root. "." for example.</param>
+        /// <param name="fileNameTemplatesFactory"></param>
 		[CakeMethodAlias]
 		public static void MakeSingleConfig(this ICakeContext context, string buildConfiguration, string configName,
 			string configDir, Func<string, string, bool, IEnumerable<string>> fileNameTemplatesFactory = null)
